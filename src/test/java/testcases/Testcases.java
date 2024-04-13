89be5e30c991b1962e97ee192c046161a95d5191
sgtesting <pdawaji@gmail.com>
2024-04-13 13:05:45 +0530

    unit test cases

---
+++ src/test/java/testcases/Testcases.java
@@ -0,0 +1,33 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class Testcases {
+
+	@Test
+	static void method1()
+	{
+		System.out.println("method1 ");
+	}
+		@Test
+	static void method2()
+	{
+		System.out.println("method2 ");
+	}
+	@Test
+	static void method3()
+	{
+		System.out.println("method3 ");
+	}
+	@Test
+	static void method4()
+	{
+		System.out.println("method4 ");
+	}
+	@Test
+	static void method5()
+	{
+		System.out.println("method5 ");
+	}
+	
+}
